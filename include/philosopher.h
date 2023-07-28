7695d5d83d7f2f6b9fdc25665dfb6ed573508be9
ADEN30 <adrien.gallet07@gmail.com>
2023-07-28 10:31:03 +0200

    controle des entrees fait

--- include/philosopher.h
+++ include/philosopher.h
@@ -6,5 +6,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+typedef struct t_arg
+{
+
+	long int	n_philo;
+	long int	t_to_die;
+	long int	t_to_eat;
+	long int	t_to_sleep;
+	long int	n_time_eat;
+} t_arg_t;
 
 #endif
--- main.c
+++ main.c
@@ -7,10 +7,76 @@ void	*routine()
 	return (0);
 }
 
-int	main(void)
+int	ft_atoi(const char *str)
 {
+	size_t	i;
+	size_t	nb;
+	int		negatif;
+
+	i = 0;
+	nb = 0;
+	negatif = 1;
+	while ((str[i] > 8 && str[i] < 14) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			negatif *= -1;
+		i++;
+	}
+	while (str[i] > 47 && str[i] < 58)
+	{
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+	return (nb * negatif);
+}
+
+void	arg_set(t_arg_t *arg, int nb)
+{
+	static int	i;
+	if (i == 0)
+		arg->n_philo = nb;
+	else if (i == 1)
+		arg->t_to_die = nb;
+	else if (i == 2)
+		arg->t_to_eat = nb;
+	else if (i == 3)
+		arg->t_to_sleep = nb;
+	else if (i == 4)
+		arg->n_time_eat = nb;
+	i++;
+}
+
+t_arg_t	*init_arg(char **argv)
+{
+	int		i;
+	t_arg_t	*arg;
+
+	i = 1;
+	arg = malloc(sizeof(t_arg_t));
+	if (!arg)
+		return (NULL);
+	while (argv[i])
+	{
+		if (ft_atoi(argv[i]) == 0)
+			return (NULL);
+		else
+			arg_set(arg, ft_atoi(argv[i]));
+		i++;
+	}
+	return (arg);
+}
+
+int	main(int argc, char **argv)
+{
+	t_arg_t		*test;
 	pthread_t	t1;
 
+	if (argc < 4)
+		return (1);	
+	test = init_arg(argv);
+	printf("%ld\n", test->n_philo);
 	pthread_create(&t1, NULL, &routine, NULL);
 	pthread_join(t1, NULL);
 	return (0);
